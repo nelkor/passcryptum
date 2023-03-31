ebfa13696f16ab4d29853efd18a33334fe0d0752
nelkor <a.tutunnik@corp.mail.ru>
2023-03-31 19:13:13 +0300

    wip

--- src/pages/work-screen/components/WorkScreen.vue
+++ src/pages/work-screen/components/WorkScreen.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { ConfigurationPanel } from '@/widgets/configuration-panel'
 import { CustomServicesList } from '@/widgets/custom-services'
+import { ShortHash } from '@/widgets/short-hash'
 import { exit } from '@/features/entry'
 </script>
 
 <template>
   <h1>Work screen</h1>
+  <ShortHash />
   <button @click="exit">Exit</button>
   <ConfigurationPanel />
   <CustomServicesList />
