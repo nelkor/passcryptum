143bbe927de1bc9684a78ced3476e1679231eda1
nelkor <a.tutunnik@corp.mail.ru>
2023-04-15 21:57:28 +0300

    reverting the erroneous commit

--- src/features/configuration-import/lib/set-configuration.ts
+++ src/features/configuration-import/lib/set-configuration.ts
@@ -1,57 +1,13 @@
 import {
-  servicesConfig,
   compressedConfigText,
   setServicesConfig,
 } from '@/entities/services-config'
 import { getMasterTool } from '@/entities/master-tool'
-import type { ServicesConfig, LoginData } from '@/entities/services-config'
+import type { ServicesConfig } from '@/entities/services-config'
 import { stringifyBuffer, getBufferOfText, encrypt } from '@/shared'
 
-const mergeConfigurations = (
-  basis: ServicesConfig,
-  added: ServicesConfig
-): ServicesConfig => {
-  const newServices: ServicesConfig = []
-
-  added.forEach(addedService => {
-    const existingService = basis.find(
-      service => service.name === addedService.name
-    )
-
-    if (!existingService) {
-      newServices.push(addedService)
-
-      return
-    }
-
-    const newLogins: LoginData[] = []
-
-    addedService.logins.forEach(addedLogin => {
-      const existingLogin = existingService.logins.find(
-        login => login.name === addedLogin.name
-      )
-
-      if (!existingLogin) {
-        newLogins.push(addedLogin)
-
-        return
-      }
-
-      if (addedLogin.version > existingLogin.version) {
-        existingLogin.version = addedLogin.version
-      }
-    })
-
-    existingService.useSpecialCharacters = addedService.useSpecialCharacters
-    existingService.passwordLengthIndex = addedService.passwordLengthIndex
-    existingService.logins = [...newLogins, ...existingService.logins]
-  })
-
-  return [...newServices, ...basis]
-}
-
-export const setConfiguration = async (config: ServicesConfig) => {
-  setServicesConfig(mergeConfigurations(servicesConfig.value, config))
+export const setConfiguration = async (servicesConfig: ServicesConfig) => {
+  setServicesConfig(servicesConfig)
 
   const { shortHash, key, iv } = getMasterTool()
   const compressedConfigBuffer = getBufferOfText(compressedConfigText.value)
