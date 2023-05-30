b5d116205079f162a86b67a7ff9afe68c8dc6a45
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2023-05-30 19:29:14 +0300

    Update vite.config.ts
    Co-authored-by: Artem <nelkor@vk.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteSingleFile } from 'vite-plugin-singlefile'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
