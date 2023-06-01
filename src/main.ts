7dd94979e7af2540c2521badba9ee1af9462e245
Artem <nelkor@vk.com>
2023-06-01 09:57:38 +0300

    Deny WebSocket (#133)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 
 import { App } from './app'
 
+Object.defineProperty(window, 'WebSocket', { value: null })
+
 createApp(App).mount('#app')
 
 if (location.host && 'serviceWorker' in navigator) {
