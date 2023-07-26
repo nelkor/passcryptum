b59864d88fef88264687f348b9dea49b56546092
nelkor <a.tutunnik@corp.mail.ru>
2023-07-26 20:28:17 +0300

    fix firefox copying

--- src/shared/ui/copy-button/CopyButton.vue
+++ src/shared/ui/copy-button/CopyButton.vue
@@ -10,18 +10,25 @@ const DELAY_TIME = 2000
 const type = 'text/plain'
 const justCopied = ref(false)
 
+const polyfillCopy = async () =>
+  navigator.clipboard.writeText(await props.getContent())
+
 const copy = () => {
   if (justCopied.value) {
     return
   }
 
-  const clipboardItem = new ClipboardItem({
-    [type]: Promise.resolve()
-      .then(props.getContent)
-      .then(result => new Blob([result], { type })),
-  })
+  if ('ClipboardItem' in window) {
+    const clipboardItem = new ClipboardItem({
+      [type]: Promise.resolve()
+        .then(props.getContent)
+        .then(result => new Blob([result], { type })),
+    })
 
-  navigator.clipboard.write([clipboardItem])
+    navigator.clipboard.write([clipboardItem])
+  } else {
+    polyfillCopy()
+  }
 
   justCopied.value = true
 
