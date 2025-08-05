098065e9b9e5f3dcc00b26484dd266d12554cdff
Василий Игнатов <vasily.ignatov@intermobility.ru>
2025-08-05 19:17:42 +0300

    feat: modernize menu structure with dividers and improved organization

--- src/widgets/app-header/components/MenuButton.vue
+++ src/widgets/app-header/components/MenuButton.vue
@@ -8,6 +8,9 @@ import {
   ColorFillOutline,
   AddCircleOutline,
   LockClosedOutline,
+  SettingsOutline,
+  DownloadOutline,
+  InformationCircleOutline,
 } from '@vicons/ionicons5'
 import { computed, h, Component } from 'vue'
 import { NDropdown, NButton, NIcon } from 'naive-ui'
@@ -39,72 +42,126 @@ const renderGitHubLabel = () =>
     'GitHub',
   )
 
-const options = computed(() => [
-  ...(props.isEntered
-    ? [
-        {
-          key: 'storage',
-          label: 'Storage',
-          icon: renderIcon(ServerOutline),
-        },
-        {
-          key: 'addService',
-          label: 'Add a service',
-          icon: renderIcon(AddCircleOutline),
-        },
-        {
-          key: 'setPin',
-          label: 'Set PIN',
-          icon: renderIcon(LockClosedOutline),
-        },
-      ]
-    : []),
-  {
-    key: 'deletePin',
-    label: 'Delete PIN',
-    disabled: isDeletePinDisabled.value,
-    icon: renderIcon(LockOpenOutline),
-  },
-  {
-    key: 'changeTheme',
-    label: renderThemeLabel,
-    icon: renderIcon(ColorFillOutline),
-  },
-  {
-    key: 'github',
-    label: renderGitHubLabel,
-    icon: renderIcon(LogoGithub),
-  },
+const options = computed(() => {
+  const mainOptions = []
+  
+  // Основные действия для авторизованных пользователей
+  if (props.isEntered) {
+    mainOptions.push(
+      {
+        key: 'addService',
+        label: 'Add Service',
+        icon: renderIcon(AddCircleOutline),
+      },
+      {
+        key: 'storage',
+        label: 'Storage',
+        icon: renderIcon(ServerOutline),
+      },
+      {
+        type: 'divider',
+        key: 'divider-1',
+      }
+    )
+  }
+  
+  // Настройки безопасности
+  mainOptions.push(
+    {
+      key: 'setPin',
+      label: 'Set PIN',
+      icon: renderIcon(LockClosedOutline),
+    },
+    {
+      key: 'deletePin',
+      label: 'Delete PIN',
+      disabled: isDeletePinDisabled.value,
+      icon: renderIcon(LockOpenOutline),
+    },
+    {
+      type: 'divider',
+      key: 'divider-2',
+    }
+  )
+  
+  // Настройки приложения
+  mainOptions.push(
+    {
+      key: 'changeTheme',
+      label: renderThemeLabel,
+      icon: renderIcon(ColorFillOutline),
+    },
+    {
+      key: 'settings',
+      label: 'Settings',
+      icon: renderIcon(SettingsOutline),
+    },
+    {
+      type: 'divider',
+      key: 'divider-3',
+    }
+  )
+  
+  // Информация и ссылки
+  mainOptions.push(
+    {
+      key: 'download',
+      label: 'Download App',
+      icon: renderIcon(DownloadOutline),
+    },
+    {
+      key: 'about',
+      label: 'About',
+      icon: renderIcon(InformationCircleOutline),
+    },
+    {
+      key: 'github',
+      label: renderGitHubLabel,
+      icon: renderIcon(LogoGithub),
+    }
+  )
+  
+  // Выход для авторизованных пользователей
+  if (props.isEntered) {
+    mainOptions.push(
+      {
+        type: 'divider',
+        key: 'divider-4',
+      },
+      {
+        key: 'signOut',
+        label: 'Sign Out',
+        icon: renderIcon(LogOutOutline),
+      }
+    )
+  }
+  
   // Временная кнопка для тестирования
-  {
-    key: 'clearSession',
-    label: 'Clear Session (Test)',
-    icon: renderIcon(LogOutOutline),
-  },
-  ...(props.isEntered
-    ? [
-        {
-          key: 'signOut',
-          label: 'Sign out',
-          icon: renderIcon(LogOutOutline),
-        },
-      ]
-    : []),
-])
+  mainOptions.push(
+    {
+      type: 'divider',
+      key: 'divider-test',
+    },
+    {
+      key: 'clearSession',
+      label: 'Clear Session (Test)',
+      icon: renderIcon(LogOutOutline),
+    }
+  )
+  
+  return mainOptions
+})
 
 const handleSelect = (key: string) => {
   switch (key) {
     case 'changeTheme':
       changeTheme()
-
       break
     case 'deletePin':
       deletePin()
-
       break
     case 'signOut':
       exit()
-
       break
     case 'clearSession':
       // Очищаем сессию для тестирования
@@ -113,15 +170,24 @@ const handleSelect = (key: string) => {
       break
     case 'setPin':
       setPin()
-
       break
     case 'addService':
       addService()
-
       break
     case 'storage':
       openStorage()
-
+      break
+    case 'settings':
+      // TODO: Добавить настройки
+      console.log('Settings clicked')
+      break
+    case 'download':
+      // TODO: Добавить ссылку на скачивание
+      console.log('Download clicked')
+      break
+    case 'about':
+      // TODO: Добавить информацию о приложении
+      console.log('About clicked')
       break
   }
 }
